b414f04235eca3af4034122917db1b06da33e03a
kuleshov-aleksei <alexxey67@gmail.com>
2023-11-11 18:00:27 +0300

    📦 Generate self-signed cert

---
+++ scripts/Generate-SelfCertPfx.ps1
@@ -0,0 +1,22 @@
+param(
+    [string]$Destination = "Watermarker_SelfSigned.pfx"
+)
+
+$CertFriendlyName = "Watermarker_SelfSigned"
+$CertPublisher = "CN=Encamy"
+$CertStoreLocation = "Cert:\CurrentUser\My"
+
+# Generate self signed cert
+$cert = New-SelfSignedCertificate `
+    -Type Custom `
+    -Subject $CertPublisher `
+    -KeyUsage DigitalSignature `
+    -FriendlyName $CertFriendlyName `
+    -CertStoreLocation $CertStoreLocation `
+    -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}")
+
+# Get size of the self signed cert
+$certificateBytes = $cert.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Pkcs12)
+
+# Save the self signed cert as a file
+[System.IO.File]::WriteAllBytes($Destination, $certificateBytes)
